3e6885d8c977be5f83a5791791e472f744c400c2
v-rohan <rohanrao.dec11@gmail.com>
2020-03-08 03:49:08 +0530

    Added Notification via alert for individual events

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -17,6 +17,7 @@ import {Calendar} from 'vue-sweet-calendar'
 import 'vue-sweet-calendar/dist/SweetCalendar.css'
 import axios from 'axios';
 
+
 export default {
   name: 'Home',
   components: {
@@ -48,21 +49,51 @@ export default {
     },
     deleteEvent(_id) {
       axios.delete(`http://localhost:3000/event?Id=${_id}`)
-        .then(res => this.events = res.data)
+        .then(res =>  { this.events = res.data
+        alert("Event has been deleted")})
         .catch(err => console.log(err));
     },
     addEvent(newEvent){
       axios.post('http://localhost:3000/event', newEvent)
         .then(res =>{ 
-          this.events = res.data})
+          alert("Event added successfully")
+          this.events = res.data
+          this.notification(res.data)})
         .catch(err => {console.log(err)});
+    },
+    notification(a){
+      a.forEach(element => {
+      var start = Date.parse(element.start);
+      var end = Date.parse(element.end);
+       var today = new Date();
+       if(today.getMonth()+1<10)
+       today= today.getFullYear()+"-0"+(today.getMonth()+1)+"-0"+today.getDate()
+       else
+      today= today.getFullYear()+"-"+(today.getMonth()+1)+"-"+today.getDate()
+       var today1= Date.parse(today);
+
+     if((today1 <= end && today1 >= start)) {
+        alert(`You've an event today named ${element.title} from ${element.start} to ${element.end}`);
+        
+    }
+      
+      });
     }
   },
-  created() {
+  mounted() {
+    this.$nextTick(function () {
     axios.get('http://localhost:3000/event')
       .then(res => {
-         this.events = res.data});
+         this.events = res.data
+          this.notification(res.data);
+         }); })
   }
+
+  
+
+
+ 
+ 
 }
 </script>
 
